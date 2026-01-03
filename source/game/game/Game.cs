32d04f65d9d5840544a0932fcbbabd77f592a9ad
boxrocket <69829964+johncosfm@users.noreply.github.com>
2026-01-03 13:18:57 -0600

    seperate game specific code from engine

---
+++ source/game/game/Game.cs
@@ -0,0 +1,9 @@
+﻿public static class Game {
+	public static Engine Engine {get; private set;} = new();
+
+	public static void Main() {
+		Engine.Init();
+		Scene.Active.Add<GameManager>();
+		Engine.Run();
+	}
+}
---
+++ source/game/game/GameManager.cs
@@ -0,0 +1,12 @@
+﻿public class GameManager : Scene.Object {
+	public override void OnCreate() {
+		Scene.Active.MainCamera = new Scene.Camera.Perspective();
+		base.OnCreate();
+	}
+
+	public Model Model;
+	public override void Render() {
+		Model.Load("models/characters/human_base.bmdl")?.Render(Transform.Indentity);
+		base.Render();
+	}
+}
